7f6e0a175333292fd3b3c6a442d4d22ce0147a1e
Christopher Lindor <chrislindor@gmail.com>
2015-06-20 00:13:29 -0400

    added comments
    Commented javascript to clarify commands

--- studentDatabase/js/searchData.js
+++ studentDatabase/js/searchData.js
@@ -1,15 +1,13 @@
 var search,
     student,
-    message = '',
-    notfound = true,
-    i;
+    message = '';
 
 //Print function 
 function print(message, id) {
     var outputDiv = document.getElementById(id);
     outputDiv.innerHTML = message;
 }
-
+//compile selected student data for display
 function getStudentReport(student) {
     var report = '<h2> Student: ' + student.name + '</h2>';
     report += '<p> Track: ' + student.track + '</p>';
@@ -17,21 +15,29 @@ function getStudentReport(student) {
     report += '<p> Achievments: ' + student.achievements + '</p>';
     return report;
 }
-
+/*
+A while loop that asks for student name or 'quit' to end program
+if a name is enterted program loops thru array of objects 
+and searchs through the student[i].name for a match
+if a match occurs calls on getStudentReport(student) 
+function to print out data
+*/
 while (true) {
     search = prompt('Type in the name of the student to search, to quit type \'quit\' ');
     if (search === null || search.toLowerCase() === 'quit') {
         print('Thank you for searching our student database', 'main');
         break;
-    }
+    }//if statment that checks if user either quit or did not enter data
     message = '';
+    var i,
+        notfound = true;
     for (i = 0; i < students.length; i += 1) {
         student = students[i];
         if (student.name.toLowerCase() === search.toLowerCase()) {
             message += getStudentReport(student);
             notfound = false;
         }
-    }
+    }//Loop to search for student data and compile report
     if (notfound) {
         message = 'Sorry  ' + search + ' is not a student here';
     }
